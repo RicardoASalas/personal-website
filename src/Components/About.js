c0f4f62e51a5f26fcd86b417634aea779b95a983
Ricardo Salas <ricardevelopment@gmail.com>
2020-08-09 10:38:12 +0200

    Changed-titles

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,12 +23,12 @@ class About extends Component {
             <img className="profile-pic"  src={profilepic} alt="Ricardo Salas Profile Pic" />
          </div>
          <div className="nine columns main-col">
-            <h2>About Me</h2>
+            <h2>Sobre Mi</h2>
 
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2>Detalles de contacto</h2>
                   <p className="address">
 						   <span>{name}</span><br />
 						   <span>{street}<br />
